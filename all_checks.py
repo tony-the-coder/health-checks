27f5f69a910acabc5374c14ccd759be9fdbab153
Tony Aldea <tonythecoder@outlook.com>
2024-04-06 19:29:06 -0400

    More practice

---
+++ all_checks.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys_exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Disk full.")
+        sys.exit(1)
+
+    print("Everything ok")
+    sys.exit(0)
+
+
+main()
